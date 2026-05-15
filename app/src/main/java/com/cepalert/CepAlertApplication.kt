3601916b62e5fb0764847c4c2743c03a1dd5423f
Albert Domenech <domenechobiol@gmail.com>
2026-05-15 21:21:44 +0200

    chore: add Hilt application class and placeholder MainActivity

---
+++ app/src/main/java/com/cepalert/CepAlertApplication.kt
@@ -0,0 +1,7 @@
+package com.cepalert
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CepAlertApplication : Application()
---
+++ app/src/main/java/com/cepalert/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cepalert
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
